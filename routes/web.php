6c6a13a6b87617bec7ca8368e9acadc2def18be3
louisa <86868898768@mail.ru>
2024-11-30 21:00:39 +0200

    create

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,13 @@ Route::group([
     Route::group(['namespace' => 'Main'], function() {
         Route::get('/', 'IndexController');
     });
+    Route::group([
+        'namespace' => 'Category',
+        'prefix'    => 'categories',
+    ], function() {
+        Route::get('/', 'IndexController')->name('admin.category.index');
+        Route::get('/create', 'CreateController')->name('admin.category.create');
+    });
 });
 
 Auth::routes();
