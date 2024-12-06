442923aaa90e205bc874735eed4d3d2c42755b2b
louisa <86868898768@mail.ru>
2024-12-06 02:43:02 +0200

    Сделан функционал заголовков постов

---
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Post;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Post\StoreRequest;
+use App\Models\Post;
+
+class StoreController extends Controller
+{
+    public function __invoke(StoreRequest $request) {
+        $data = $request->validated();
+        Post::firstOrCreate($data);
+        // firstOrCreate( [по какому ключу происходит проверка ], [если новое значение то какие атрибуты должны появится в бд])
+        // firstOrCreate( ['title' => $data['title']],            ['title' => $data['title']])
+        // если атрибут проверки и добавления равны можно писать:
+        // firstOrCreate([ 'title' => $data['title']])
+        // если ключ называется как надо можно сократить до $data
+
+        return redirect()->route('admin.post.index');
+    }
+}
