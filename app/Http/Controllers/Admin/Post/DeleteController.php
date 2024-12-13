6cddffda99504a26e15a966f1f1b527eb8772a54
louisa <86868898768@mail.ru>
2024-12-13 21:49:49 +0200

    Post soft delete

--- app/Http/Controllers/Admin/Post/DeleteController.php
+++ app/Http/Controllers/Admin/Post/DeleteController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Post\UpdateRequest;
+use App\Http\Requests\Admin\Category\UpdateRequest;
+use App\Models\Category;
 use App\Models\Post;
 
 class DeleteController extends Controller
