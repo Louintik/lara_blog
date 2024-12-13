80708053a104d6439802918c52607212cf5d26a0
louisa <86868898768@mail.ru>
2024-12-13 22:07:43 +0200

    Post soft delete доделывание

--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -14,8 +14,14 @@ class UpdateController extends Controller
         $tadIds = $data['tag_ids'];
         unset($data['tag_ids']);
 
+        if (isset($data['main_image'])){
         $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-        $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+        }
+
+        if(isset($data['preview_image'])){
+            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+        }
+
         $post->update($data);
         $post->tags()->sync($tadIds); //метод sync удаляет из бд все привязки у поста с тегами и добавляет только указанные в скобках
         return view('admin.post.show', compact('post' )); //передаем чтобы поля были заполнены
