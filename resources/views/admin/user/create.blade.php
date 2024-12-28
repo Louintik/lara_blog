b1c248770d733703c70443f40cecab6fc5f68124
louisa <86868898768@mail.ru>
2024-12-28 17:06:24 +0200

    CRUD пользователей

---
+++ resources/views/admin/user/create.blade.php
@@ -0,0 +1,68 @@
+@extends('admin.layouts.main ')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Добавление пользователя</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Добавление пользователя</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <section class="content">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-12">
+                        <form action=" {{ route('admin.user.store') }}" method="POST" class="w-25">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="name" placeholder="Имя пользователя">
+                                @error('name')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" placeholder="Электронная почта">
+                                @error('email')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="password" placeholder="Пароль">
+                                @error('password ')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Выберите роль</label>
+                                <select name="role" class="form-control">
+                                    @foreach($roles as $id => $role)
+                                        {{-- ключ $id это у нас 0 или 1, а $role это админ или читатель из статического метода getRoles() в модели User--}}
+                                        <option value="{{ $id }}"
+                                            {{ $id == old('role')? ' selected' : '' }}>
+                                            {{-- чтобы выбранное значение в поле не скидывалось при неудаче--}}
+                                            {{ $role }}</option>
+                                    @endforeach
+                                </select>
+                                @error('role')
+                                <div class="text-danger">{{ $message }} </div>
+                                @enderror
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Добавить">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
